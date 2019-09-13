6cd055c950b42e5d1084a2130bb01904a7591e1c
Rosen <rr12520@bristol.ac.uk>
2019-09-13 13:30:34 +0100

    new script

---
+++ stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+fin = open(sys.argv[1],'r')
+fout = open(sys.argv[2],'w')
+for line in fin:
+     fout.write(line)
+
+fin.close()
+fout.close()
+     
+
+
