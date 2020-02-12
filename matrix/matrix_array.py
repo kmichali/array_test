794c89620fe36cee007f498051ffcd0ed8d160bb
Rosen <rr12520@bristol.ac.uk>
2020-02-12 17:44:36 +0000

    read PBS_ARRAY_INDEX only once

--- matrix/matrix_array.py
+++ matrix/matrix_array.py
@@ -4,11 +4,12 @@ import os
 input = np.loadtxt("input.txt", dtype='f', delimiter=',')
 print(input)
 counter = 0
+index = int(os.environ['PBS_ARRAY_INDEX'])
+
 
 for i in range(0,input.shape[0]):
     for j in range(0, input.shape[1]):
         counter = counter+1
         print('counter:', counter)
-        index = int(os.environ['PBS_ARRAY_INDEX'])
         if counter == index:
             print('processing element', input[i,j])
